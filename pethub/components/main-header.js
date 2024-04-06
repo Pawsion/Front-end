d6fd86929c212fcf1d2f09bd6b4d59408415c19b
Radivoj Putic <raleputic@gmail.com>
2024-04-06 16:52:35 +0200

    Header improvement

--- pethub/components/main-header.js
+++ pethub/components/main-header.js
@@ -1,9 +1,23 @@
+import Link from "next/link";
+import logoImage from "@/assets/images/logoImage.png";
+
 export default function MainHeader() {
-    return(
-        <header>
-            <nav>
-                <h1>This is header</h1>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="bg-mainColorBlue py-4 px-8 flex justify-between items-center">
+      <Link href="/">
+        <img src={logoImage.src} alt="Pethub image" />
+      </Link>
+      <nav className="flex space-x-4 text-mainColorWhite">
+        <Link href="/pansioni">Pansioni</Link>
+        <Link href="/saloni">Saloni</Link>
+        <Link href="/azili">Azili</Link>
+        <Link href="/imena">Imena</Link>
+        <Link href="/perionice">Perionice</Link>
+        <Link href="/dresura">Dresura</Link>
+        <Link href="/parkici">Parkići</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/login">Login</Link>
+      </nav>
+    </header>
+  );
 }
--- pethub/tailwind.config.js
+++ pethub/tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        mainColorBlue: '#575FCC',
+        mainColorWhite: '#FEF9F3',
       },
     },
   },
+  variants: {},
   plugins: [],
 };
