0a2a2226c6a9ef3a61eca9e49434409c8e2977e8
Radivoj Putic <raleputic@gmail.com>
2024-04-21 00:22:29 +0200

    aligned buttons in the header

--- pethub/components/Home/buttonKlikinder.js
+++ pethub/components/Home/buttonKlikinder.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ButtonKlikinder() {
   return (
     <button
-      className="bg-mainColorBlue text-mainColorWhite py-4 px-10 rounded-full mt-8 font-Bogart w-56 ml-48"
+      className="bg-mainColorBlue text-mainColorWhite py-4 px-10 rounded-full mt-16 font-Bogart w-56 ml-56"
     >
       Kliknider!
     </button>
