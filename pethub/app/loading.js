280ea52c22dd02c7095d52df87495da54daeafc7
Radivoj Putic <raleputic@gmail.com>
2024-04-29 20:28:00 +0200

    added different animation for loading page

--- pethub/app/loading.js
+++ pethub/app/loading.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import logoImage from "@/assets/images/logoImage.png";
+
 export default function HeroLoadingPage() {
     return (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-200">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+            <Link href="/">
+                <img
+                    src={logoImage.src}
+                    alt="Pethub logo"
+                    className="h-12 md:h-16 lg:h-20 animate-pulse"
+                />
+            </Link>
         </div>
     );
 }
