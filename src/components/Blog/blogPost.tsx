e2dad1cadf75cc04c315067bff3a56d358e6ac34
milosdjurica <milosdjurica13@gmail.com>
2024-08-26 20:35:42 +0200

    styling

--- src/components/Blog/blogPost.tsx
+++ src/components/Blog/blogPost.tsx
@@ -10,19 +10,22 @@ export default function BlogPost({
   link,
 }: BlogData) {
   return (
-    <Link
-      href={link}
-      className="flex flex-col
-    lg:flex-row
-    xl:mx-36"
-    >
-      <div className="relative h-[300px] md:h-[500px] lg:h-[600px] lg:w-[50%]">
-        <Image alt="Citati image" src={image} className="object-cover" fill />
+    <Link href={link} className="flex flex-col lg:flex-row xl:mx-36">
+      <div
+        className="relative h-[300px]  
+        md:h-[500px] lg:h-[600px] lg:w-[50%]"
+      >
+        <Image
+          alt="Citati image"
+          src={image}
+          className="border border-red-700 object-cover 
+          lg:rounded-s-3xl xl:rounded-s-full"
+          fill
+        />
       </div>
       <div
-        className="flex flex-col justify-start space-y-6 p-4
-       text-lg text-gray-600 lg:w-1/2
-       lg:space-y-20"
+        className="p-4text-lg flex flex-col justify-start space-y-6 text-gray-600 
+        lg:w-1/2 lg:space-y-20"
       >
         <h4 className="text-xl ">{timeToRead}</h4>
         <h1 className="text-4xl font-bold lg:text-5xl">{title}</h1>
