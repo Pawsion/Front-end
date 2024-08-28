91ae0b7bb9e911f4a2437d76170f5979851d6c2a
Radivoj Putic <raleputic@gmail.com>
2024-08-28 14:16:52 +0200

    started food

---
+++ src/app/blog/food/page.tsx
@@ -0,0 +1,12 @@
+import WashHeader from '@/components/Wash/washHeader'; 
+import Footer from '@/components/Footer/footer';
+import React from 'react';
+import FoodHeader from '@/components/Food/foodHeader';
+
+export default function WashPage() {
+  return (
+    <><FoodHeader />
+    <Footer />
+    </>
+  );
+}
