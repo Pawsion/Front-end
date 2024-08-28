9b7add0aa20cc3d927815257d6e7b911034b7f3c
milosdjurica <milosdjurica13@gmail.com>
2024-08-28 23:39:55 +0200

    second section finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import CorgiImage from "@/components/Home/Home2/corgiPicture";
-import TitleComponentSecond from "@/components/Home/Home2/titleComponent";
-import SubtitleComponentSecond from "@/components/Home/Home2/subtitleComponent";
 import TitleComponentThird from "@/components/Home/Home3/titleComponent";
 import SubtitleComponentThird from "@/components/Home/Home3/subtitleComponent";
 import SubtitleComponentApplication from "@/components/Home/Home3/subtitleApplication";
@@ -9,26 +7,15 @@ import ButtonComponent3 from "@/components/Home/Home3/button";
 import DogPicuteThird from "@/components/Home/Home3/dogPicture";
 import Footer from "@/components/Footer/footer";
 import FirstSection from "@/components/Home/firstSection";
+import SecondSection from "@/components/Home/secondSection";
 
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <main className="body gradient relative">
       <FirstSection />
-      {/* <section className="mb-48">
-        <OrangePawComponent />
-        <TitleComponent />
-        <SubtitleComponent />
-        <PurplePawComponent />
-        <DogImage />
-      </section> */}
-      <section className="h-[92vh] bg-pageColorTan">
-        <div>
-          <CorgiImage />
-          <TitleComponentSecond />
-          <SubtitleComponentSecond />
-        </div>
-      </section>
+      <SecondSection />
+
       <section className="flex h-[calc(100vh-60px)] flex-col items-center pt-8">
         <div className="mt-8 flex w-full flex-col items-center">
           <TitleComponentThird />
