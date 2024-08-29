8b1032907d9f83c065bf92b6086e65c06329919d
milosdjurica <milosdjurica13@gmail.com>
2024-08-29 14:57:50 +0200

    thirdSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import TitleComponentThird from "@/components/Home/Home3/titleComponent";
-import SubtitleComponentThird from "@/components/Home/Home3/subtitleComponent";
-import SubtitleComponentApplication from "@/components/Home/Home3/subtitleApplication";
-import ButtonComponent3 from "@/components/Home/Home3/button";
-import DogPicuteThird from "@/components/Home/Home3/dogPicture";
 import FirstSection from "@/components/Home/firstSection";
 import SecondSection from "@/components/Home/secondSection";
+import ThirdSection from "@/components/Home/thirdSection";
 
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -13,16 +9,17 @@ export default async function Home() {
     <main className="body gradient relative text-center lg:text-start">
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
 
-      <section className="flex h-[calc(100vh-60px)] flex-col items-center pt-8">
-        <div className="mt-8 flex w-full flex-col items-center">
+      {/* <section className="flex h-[calc(100vh-60px)] flex-col items-center pt-8">
+        <div className="relative mt-8 flex w-full flex-col items-center">
           <TitleComponentThird />
           <SubtitleComponentThird />
           <SubtitleComponentApplication />
           <ButtonComponent3 />
           <DogPicuteThird />
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
