c7be9dc6c9d401e32fc7a21ff29fb92212ac3222
milosdjurica <milosdjurica13@gmail.com>
2024-09-12 13:18:30 +0200

    fixed for build, added veterinari page

---
+++ src/app/veterinari/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function VeterinariPage() {
+  return (
+    <div>
+      <h2>Veterinari Page</h2>
+    </div>
+  );
+}
--- src/components/Food/foodList.tsx
+++ src/components/Food/foodList.tsx
@@ -1,99 +1,188 @@
-import React from 'react';
+import React from "react";
 
 export default function FoodList() {
   return (
-    <div id="lista-najbolje-hrane" className="mb-32 ml-64 text-[28px] font-openSans font-regular">
-      <h2 className="text-[64px] font-regular text-mainColorBlack text-left mb-16">
-        Lista 10 najbolje hrane po brendu/proizvođaču<br />po našem mišljenju
+    <div
+      id="lista-najbolje-hrane"
+      className="font-openSans mb-32 ml-64 text-[28px] font-regular"
+    >
+      <h2 className="text-mainColorBlack mb-16 text-left text-[64px] font-regular">
+        Lista 10 najbolje hrane po brendu/proizvođaču
+        <br />
+        po našem mišljenju
       </h2>
       <p className="mb-8">
-        Različita su mišljenja po pitanju toga koji brend pseće hrane je najbolji. Mnogi sajtovi navode proizvođače koji<br />su ili sponzori, ili prodaju njihove proizvode. Iako ćemo vam nabrojati nekoliko brendova, želeli bismo vam da<br />napomenemo da sigurno ima još kvalitetnih brendova i da vam damo par smernica pri odabiru proizvoda.
+        Različita su mišljenja po pitanju toga koji brend pseće hrane je
+        najbolji. Mnogi sajtovi navode proizvođače koji
+        <br />
+        su ili sponzori, ili prodaju njihove proizvode. Iako ćemo vam nabrojati
+        nekoliko brendova, želeli bismo vam da
+        <br />
+        napomenemo da sigurno ima još kvalitetnih brendova i da vam damo par
+        smernica pri odabiru proizvoda.
       </p>
       <p className="mb-8">
-        Ono što vam preporučujemo jeste da uzimate hranu označenu kao premium ili superpremium. Pri odabiru<br />proizvoda verovatno će igrati ulogu i cena, tako da ako niste u mogućnosti da priuštite vašem ljubimcu<br />superpremium hranu, ne očajavajte. Premium hrana je isto dobra, ali ćete shodno zdravlju, starosti i načinu<br />života vašeg psa davati i dodatne suplemente po potrebi. U većini slučajeva to ni neće biti potrebno.
+        Ono što vam preporučujemo jeste da uzimate hranu označenu kao premium
+        ili superpremium. Pri odabiru
+        <br />
+        proizvoda verovatno će igrati ulogu i cena, tako da ako niste u
+        mogućnosti da priuštite vašem ljubimcu
+        <br />
+        superpremium hranu, ne očajavajte. Premium hrana je isto dobra, ali ćete
+        shodno zdravlju, starosti i načinu
+        <br />
+        života vašeg psa davati i dodatne suplemente po potrebi. U većini
+        slučajeva to ni neće biti potrebno.
       </p>
       <p className="mb-8">
-        Ova lista nije sponzorisana, već je napravljena na osnovu subjektivnog mišljenja našeg veterinara.
+        Ova lista nije sponzorisana, već je napravljena na osnovu subjektivnog
+        mišljenja našeg veterinara.
       </p>
       <div className="text-gray-700">
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           1. Acana
         </h3>
         <p className="mb-4">
-          Ukoliko se odlučite da dajete ovu superpremium hranu, nećete zažaliti. Pse hranjene Acanom uglavnom<br />odlikuje sjajna dlaka, posledica kvalitetnih sastojaka, od kojih čini minimum 50% animalnog porekla, kao i<br />celo voće i povrće, vitamini, minerali i probiotici. U okviru ovog brenda hrana je podeljena na različite<br />veličine i starosne kategorije pasa, kao i na vrstu mesa koja se nalazi u njoj.
+          Ukoliko se odlučite da dajete ovu superpremium hranu, nećete zažaliti.
+          Pse hranjene Acanom uglavnom
+          <br />
+          odlikuje sjajna dlaka, posledica kvalitetnih sastojaka, od kojih čini
+          minimum 50% animalnog porekla, kao i<br />
+          celo voće i povrće, vitamini, minerali i probiotici. U okviru ovog
+          brenda hrana je podeljena na različite
+          <br />
+          veličine i starosne kategorije pasa, kao i na vrstu mesa koja se
+          nalazi u njoj.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           2. Royal Canin
         </h3>
         <p className="mb-4">
-          Bez Royal Canina ova lista ne bi bila potpuna. Pored kvalitetnih sastojaka, ovaj brend odlikuje to što su<br />proizvodi podeljeni ne samo po starosnim kategorijama i veličini psa, već postoji i hrana posebno napravljena<br />za neke rase. Takođe, Royal Canin poznat je i po medicinskoj liniji proizvoda, koja se pokazala veoma<br />učinkovitom.
+          Bez Royal Canina ova lista ne bi bila potpuna. Pored kvalitetnih
+          sastojaka, ovaj brend odlikuje to što su
+          <br />
+          proizvodi podeljeni ne samo po starosnim kategorijama i veličini psa,
+          već postoji i hrana posebno napravljena
+          <br />
+          za neke rase. Takođe, Royal Canin poznat je i po medicinskoj liniji
+          proizvoda, koja se pokazala veoma
+          <br />
+          učinkovitom.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           3. Hills
         </h3>
         <p className="mb-4">
-          Još jedan poznat brend koji nudi kvalitetnu hranu. Isto kao i Royal Canin, Hills nudi proizvode kategorizovane<br />na osnovu veličine i starosti psa, kao i medicinsku liniju.
+          Još jedan poznat brend koji nudi kvalitetnu hranu. Isto kao i Royal
+          Canin, Hills nudi proizvode kategorizovane
+          <br />
+          na osnovu veličine i starosti psa, kao i medicinsku liniju.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           4. Farmina N&D
         </h3>
         <p className="mb-4">
-          Ovaj brend odlikuje se raznovrsnošću proizvoda koja ume biti zbunjujuća za vlasnike. Kao jedan od glavnih<br />sastojaka, pored mesa, mogu se naći kivi, nar, narandža, tropsko voće, kokos i bundeva. Pretpostavljamo da<br />razlog ovome leži u činjenici da se proizvod pravi tako da se svidi vlasnicima.<br />
+          Ovaj brend odlikuje se raznovrsnošću proizvoda koja ume biti
+          zbunjujuća za vlasnike. Kao jedan od glavnih
+          <br />
+          sastojaka, pored mesa, mogu se naći kivi, nar, narandža, tropsko voće,
+          kokos i bundeva. Pretpostavljamo da
+          <br />
+          razlog ovome leži u činjenici da se proizvod pravi tako da se svidi
+          vlasnicima.
+          <br />
         </p>
         <p className="mb-4 mt-8">
-          N&D sadrži kvalitetne sastojke, ali je preporučljivo da se odabere samo jedan proizvod koji će pas jesti. Kao i<br />prethodno navedeni brendovi i N&D kategorizuje svoju hranu u zavisnosti od uzrasta i veličine psa.
+          N&D sadrži kvalitetne sastojke, ali je preporučljivo da se odabere
+          samo jedan proizvod koji će pas jesti. Kao i<br />
+          prethodno navedeni brendovi i N&D kategorizuje svoju hranu u
+          zavisnosti od uzrasta i veličine psa.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           5. Farmina Cibau
         </h3>
         <p className="mb-4">
-          Pored "obične" linije proizvoda, Cibau izdvaja i "sensitive" kategorija namenjena alergičnimi psima sa<br />osetljivim digestivnim traktom.
+          Pored &quot;obične&quot; linije proizvoda, Cibau izdvaja i
+          &quot;sensitive&quot; kategorija namenjena alergičnimi psima sa
+          <br />
+          osetljivim digestivnim traktom.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           6. Brit Care
         </h3>
         <p className="mb-4">
-          U pitanju je još jedan brend koji se pokazao učinkovit kod alergičnih pasa. Pored "Sensitive" kategorije,<br />preporučujemo "Weight loss" i "Endurance".
+          U pitanju je još jedan brend koji se pokazao učinkovit kod alergičnih
+          pasa. Pored &quot;Sensitive&quot; kategorije,
+          <br />
+          preporučujemo &quot;Weight loss&quot; i &quot;Endurance&quot;.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           7. Trovet
         </h3>
         <p className="mb-4">
-          Trovetovi proizvodi pripadaju medicinskoj kategoriji hrane za pse. Ono što bismo izdvojili jeste raznolikost u<br />hipoalergijskim proizvodima sačinjenim od različitih monoproteinskih izvora. Takođe, Recovery liquid se<br />pokazao veoma učinkovitim tokom oporavka životinja nakon operacija i od mnogih težkih oboljenja.
+          Trovetovi proizvodi pripadaju medicinskoj kategoriji hrane za pse. Ono
+          što bismo izdvojili jeste raznolikost u<br />
+          hipoalergijskim proizvodima sačinjenim od različitih monoproteinskih
+          izvora. Takođe, Recovery liquid se
+          <br />
+          pokazao veoma učinkovitim tokom oporavka životinja nakon operacija i
+          od mnogih težkih oboljenja.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           8. Specific
         </h3>
         <p className="mb-4">
-          Kod ovog brenda pohvalili bismo medicinsku liniju proizvoda i izdvojili Allergen Management i Allergen<br />Management plus kao izuzetno efikasne kod pasa sa alergijskim problemima.
+          Kod ovog brenda pohvalili bismo medicinsku liniju proizvoda i
+          izdvojili Allergen Management i Allergen
+          <br />
+          Management plus kao izuzetno efikasne kod pasa sa alergijskim
+          problemima.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           9. Alleva
         </h3>
         <p className="mb-4">
-          Alleva sadrži proizvode bez žitarica, bez glutena, sa niskim sadržajem žitarica, prirodnim antioksidansima i<br />monoproteinima.
+          Alleva sadrži proizvode bez žitarica, bez glutena, sa niskim sadržajem
+          žitarica, prirodnim antioksidansima i<br />
+          monoproteinima.
         </p>
 
-        <h3 className="text-[36px] font-regular italic text-mainColorBlue mb-24 mt-24">
+        <h3 className="mb-24 mt-24 text-[36px] font-regular italic text-mainColorBlue">
           10. Trainer
         </h3>
         <p className="mb-4">
-          Još jedan dobro poznat, ali i kvalitetan brend u Srbiji. Psi ga rado jedu, a izdvojili bismo Sensitiv No Gluten<br />proizvod.
+          Još jedan dobro poznat, ali i kvalitetan brend u Srbiji. Psi ga rado
+          jedu, a izdvojili bismo Sensitiv No Gluten
+          <br />
+          proizvod.
         </p>
       </div>
       <p className="mt-16">
-        Na kraju, važno je zapamtiti da ne postoji univerzalno "najbolja" hrana za pse, već je najvažnije pronaći onu<br />koja odgovara individualnim potrebama vašeg psa. Budite sigurni da se konsultujete s veterinarom pre nego<br />što promenite ishranu svog psa, posebno ako ima posebne zdravstvene potrebe ili alergije.
+        Na kraju, važno je zapamtiti da ne postoji univerzalno
+        &quot;najbolja&quot; hrana za pse, već je najvažnije pronaći onu
+        <br />
+        koja odgovara individualnim potrebama vašeg psa. Budite sigurni da se
+        konsultujete s veterinarom pre nego
+        <br />
+        što promenite ishranu svog psa, posebno ako ima posebne zdravstvene
+        potrebe ili alergije.
       </p>
       <p className="mt-16">
-        Istražite različite opcije hrane za pse i čitajte sastojke kako biste se osigurali da birate kvalitetnu hranu koja<br />odgovara prehrambenim potrebama vašeg ljubimca. Takođe, ključno je pratiti reakciju vašeg psa na novu<br />hranu i prilagoditi ishranu prema njegovim potrebama.
+        Istražite različite opcije hrane za pse i čitajte sastojke kako biste se
+        osigurali da birate kvalitetnu hranu koja
+        <br />
+        odgovara prehrambenim potrebama vašeg ljubimca. Takođe, ključno je
+        pratiti reakciju vašeg psa na novu
+        <br />
+        hranu i prilagoditi ishranu prema njegovim potrebama.
       </p>
     </div>
   );
