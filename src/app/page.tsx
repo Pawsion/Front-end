a515da08ea6f6ca30b75c1e24cad0254bb111c5d
milosdjurica <milosdjurica13@gmail.com>
2024-09-13 14:09:56 +0200

    2nd section home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default async function Home() {
         />
       </div>
       <FirstSection />
-      {/* <SecondSection />
-      <ThirdSection /> */}
+      <SecondSection />
+      {/* <ThirdSection /> */}
     </main>
   );
 }
