c353c117b0dccab47ed0554eedcde4d757dfc472
milosdjurica <milosdjurica13@gmail.com>
2024-09-13 13:34:44 +0200

    first home component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import FirstSection from "@/components/Home/firstSection";
 import SecondSection from "@/components/Home/secondSection";
 import ThirdSection from "@/components/Home/thirdSection";
+import PagesFirstSection from "@/components/Reusable/pages-first-section";
+import bgImage from "@/../public/assets/images/Saloni/Background.png";
 
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
-    <main className="body gradient relative text-center lg:text-start">
+    <main className="gradient text-center lg:text-start">
+      <div>
+        <PagesFirstSection
+          title="PetHub - usluge i saveti za pse koji će vam olakšati život"
+          bgImage={bgImage}
+        />
+      </div>
       <FirstSection />
-      <SecondSection />
-      <ThirdSection />
+      {/* <SecondSection />
+      <ThirdSection /> */}
     </main>
   );
 }
