a15c1b750be79f0881cb5935bec0ba4e0fdfe71d
Radivoj Putic <raleputic@gmail.com>
2024-10-18 10:42:57 +0200

    bugfix home page and login

--- src/components/Reusable/pages-first-section.tsx
+++ src/components/Reusable/pages-first-section.tsx
@@ -1,22 +1,21 @@
 import Image, { StaticImageData } from "next/image";
 
 export default function PagesFirstSection({
+  title,
   bgImage,
 }: {
+  title: string;
   bgImage: StaticImageData;
 }) {
   return (
     <div className="relative flex min-h-[70vh] bg-[#F5F5F7] pl-10 lg:pl-20">
-      <h1 className="z-[1] my-auto w-full text-center text-5xl font-medium  
-          md:text-5xl lg:text-8xl md:w-1/2 md:max-w-[50%] lg:w-3/4 lg:text-start lg:ml-40">
-        <span className="text-activeLinkGold">PetHub</span>
-        <span className="text-mainColorBlue"> - usluge i</span>
-        <br />
-        <span className="text-mainColorBlue">
-          saveti za pse koji će<br />
-          vam olakšati život
-        </span>
+      <h1
+        className="z-[1] my-auto w-3/4 text-center text-5xl font-semibold 
+        text-mainColorBlue  md:w-1/2 md:max-w-[50%] lg:w-3/4 lg:text-start"
+      >
+        {title}
       </h1>
+      {/* //! TODO -> Put it absolute and text over it on SM and MD  */}
       <Image
         src={bgImage}
         alt="Background dog"
