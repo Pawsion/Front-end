67f22e9012147577a5766749e17b55ce020d0b96
Radivoj Putic <raleputic@gmail.com>
2024-11-23 15:36:40 +0100

    vets responsive

--- src/components/Veterinari/description.tsx
+++ src/components/Veterinari/description.tsx
@@ -2,29 +2,31 @@ export default function Description() {
   return (
     <div className="m-auto w-3/4">
       <div className="space-y-20">
-        <div
-          className="text-2xl mt-32 text-regular"
-        >
+        <div className="lg:mt-32 mt-16 text-regular text-base lg:text-2xl leading-relaxed">
           <p>
-          Veterinar  je medicinski stručnjak specijalizovan za dijagnostiku, lečenje i prevenciju bolesti kod životinja.<br />
-          Pored toga što prepisuju tretmane za lečenje uobičajenih bolesti kao što su npr. infekcije uha, mogu vam<br /> 
-          pružiti
-          neprocenjive savete o opštoj nezi, ishrani ili načinu vežbanja koji će pomoći vašim ljubimcima.<br />
+            Veterinar je medicinski stručnjak specijalizovan za dijagnostiku, lečenje i prevenciju bolesti kod životinja.
+            <br className="hidden sm:inline" />
+            Pored toga što prepisuju tretmane za lečenje uobičajenih bolesti kao što su npr. infekcije uha, mogu vam
+            <br className="hidden sm:inline" />
+            pružiti neprocenjive savete o opštoj nezi, ishrani ili načinu vežbanja koji će pomoći vašim ljubimcima.
+            <br className="hidden sm:inline" />
           </p>
           <p className="mt-16">
-          Kako bi pružio sveobuhvatnu negu svojim pacijentima, često sarađuje zajedno sa drugim profesionalcima, kao<br />
-          što su treneri i nutricionisti. 
+            Kako bi pružio sveobuhvatnu negu svojim pacijentima, često sarađuje zajedno sa drugim profesionalcima, kao
+            <br className="hidden sm:inline" />
+            što su treneri i nutricionisti.
           </p>
           <div className="mt-24">
-            <h2>
-            Kako pronaći veterinara?
+            <h2 className="lg:text-6xl text-2xl">
+              Kako pronaći veterinara?
             </h2>
-            <p className="mt-24">
-            Pronalaženje veterinara je ključno za zdravlje i dobrobit vašeg ljubimca. Kao kad se odlučujete za bilo koju<br />
-            vrstu lekara za vas ili člana vaše porodice, i ovde morate istražiti pre nego što odaberete.
+            <p className="lg:mt-24 mt-16">
+              Pronalaženje veterinara je ključno za zdravlje i dobrobit vašeg ljubimca. Kao kad se odlučujete za bilo koju
+              <br className="hidden sm:inline" />
+              vrstu lekara za vas ili člana vaše porodice, i ovde morate istražiti pre nego što odaberete.
             </p>
             <p className="mt-16">
-            Evo nekoliko saveta koji će vam pomoći u tom procesu:
+              Evo nekoliko saveta koji će vam pomoći u tom procesu:
             </p>
           </div>
         </div>
